f853f58b811cd74bcb837a3f0273c72f5d5001ea
Ben <bensnonworkemail@gmail.com>
2016-08-01 17:30:40 -0700

    Add files via upload
    -Updated the pokemonInGame array. Moved to after the variable creation. Still need to move the variables inside the array.
    -Added a very quick catchPokemon(), which creates a pokemon, lets the user name it, and pushes it to trainerCollection.
    - Added a displayPokemon() that takes an array of pokemon and displays their species, trainer name, level, and XP to next level.

--- pokemongame.js
+++ pokemongame.js
@@ -1,7 +1,5 @@
-var pokemonInGame = [Bulbasaur, Charmander, Squirtle, Caterpie, Weedle, Pidgey, Ekans, Sandshrew, Oddish, Psyduck, Poliwag, Abra, Machop];
-
 var Bulbasaur = {
-	name: "Bulbasaur",
+	species: "Bulbasaur",
 	id: 001,
 	type: "grass",
 	evolvesTo: Bulbasaur,
@@ -12,7 +10,7 @@ var Bulbasaur = {
 };
 
 var Charmander = {
-	name: "Charmander",
+	species: "Charmander",
 	id: 004,
 	type: "fire",
 	evolvesTo: Charmander,
@@ -24,7 +22,7 @@ var Charmander = {
 };
 
 var Squirtle = {
-	name: "Squirtle",
+	species: "Squirtle",
 	id: 007,
 	type: "water",
 	evolvesTo: Squirtle,
@@ -36,7 +34,7 @@ var Squirtle = {
 };
 
 var Caterpie = {
-	name: "Caterpie",
+	species: "Caterpie",
 	id: 010,
 	type: "bug",
 	evolvesTo: Caterpie,
@@ -48,7 +46,7 @@ var Caterpie = {
 };
 
 var Weedle = {
-	name: "Weedle",
+	species: "Weedle",
 	id: 013,
 	type: "bug",
 	evolvesTo: Weedle,
@@ -60,7 +58,7 @@ var Weedle = {
 };
 
 var Pidgey = {
-	name: "Pidgey",
+	species: "Pidgey",
 	id: 016,
 	type: "flying",
 	evolvesTo: Pidgey,
@@ -72,7 +70,7 @@ var Pidgey = {
 };
 
 var Ekans = {
-	name: "Ekans",
+	species: "Ekans",
 	id: 023,
 	type: "poison",
 	evolvesTo: Ekans,
@@ -84,7 +82,7 @@ var Ekans = {
 };
 
 var Sandshrew = {
-	name: "Sandshrew",
+	species: "Sandshrew",
 	id: 027,
 	type: "ground",
 	evolvesTo: Sandshrew,
@@ -96,7 +94,7 @@ var Sandshrew = {
 };
 
 var Oddish = {
-	name: "Oddish",
+	species: "Oddish",
 	id: 043,
 	type: "grass",
 	evolvesTo: Oddish,
@@ -108,7 +106,7 @@ var Oddish = {
 };
 
 var Psyduck = {
-	name: "Psyduck",
+	species: "Psyduck",
 	id: 054,
 	type: "water",
 	evolvesTo: Psyduck,
@@ -120,7 +118,7 @@ var Psyduck = {
 };
 
 var Poliwag = {
-	name: "Poliwag",
+	species: "Poliwag",
 	id: 060,
 	type: "water",
 	evolvesTo: Poliwag,
@@ -132,7 +130,7 @@ var Poliwag = {
 };
 
 var Abra = {
-	name: "Abra",
+	species: "Abra",
 	id: 063,
 	type: "psychic",
 	evolvesTo: Abra,
@@ -144,7 +142,7 @@ var Abra = {
 };
 
 var Machop = {
-	name: "Machop",
+	species: "Machop",
 	id: 066,
 	type: "fighting",
 	evolvesTo: Machop,
@@ -155,32 +153,99 @@ var Machop = {
 	XPperLevel: 15
 };
 
-function createPokemon(pokemonName) {
-		if (pokemonName) {
-			var newPokemonIf = makeChosenPokemon(pokemonName);
+var pokemonInGame = [Bulbasaur, Charmander, Squirtle, Caterpie, Weedle, Pidgey, Ekans, Sandshrew, Oddish, Psyduck, Poliwag, Abra, Machop];
+var trainerCollection = [];
+
+/* takes the name of a pokemon Object and returns a newly created low-level pokemon with randomized stats. 
+If no pokemon object is provided, then the game returns a newly created random pokemon by picking from the 
+pokemonInGame array */ 
+function createPokemon(pokemonObject) {
+		if (pokemonObject) {
+			var newPokemonIf = makeChosenPokemon(pokemonObject);
 			return newPokemonIf;
 		} else {
-			var pokemonArray = [Bulbasaur, Charmander, Squirtle, Caterpie, Weedle, Pidgey, Ekans, Sandshrew, Oddish, Psyduck, Poliwag, Abra, Machop];
-			var a =  Math.floor(Math.random() * pokemonArray.length);
-			var newPokemonElse = makeChosenPokemon(pokemonArray[a]);
+			var a =  Math.floor(Math.random() * pokemonInGame.length);
+			var newPokemonElse = makeChosenPokemon(pokemonInGame[a]);
 			return newPokemonElse;		
 	} 
 }
 
-function makeChosenPokemon(pokemonName) {
+//Takes a pokemon Object as input and returns a newly created low-level pokemon with randomized stats. The meat of createPokemon()
+function makeChosenPokemon(pokemonObject) {
 	var ranNum =  Math.floor(Math.random() * 5);
 	var newPokemon = {
-		name: pokemonName.name,
-		id: pokemonName.id,
-		type: pokemonName.type,
-		evolvesTo: pokemonName.evolvesTo,
-		level: pokemonName.baseLevel + ranNum,
-		HP: pokemonName.baseHP + (ranNum * pokemonName.HPperLevel) + (Math.floor(Math.random() * 5))
+		species: pokemonObject.species,
+		id: pokemonObject.id,
+		type: pokemonObject.type,
+		evolvesTo: pokemonObject.evolvesTo,
+		level: pokemonObject.baseLevel + ranNum,
+		HP: pokemonObject.baseHP + (ranNum * pokemonObject.HPperLevel) + (Math.floor(Math.random() * 5)),
+		XPtoNextLevel: pokemonObject.XPperLevel * (ranNum + pokemonObject.baseLevel)
 	}
 	return newPokemon;
 };
+
+//Saves a randomly created pokemon, lets the user name it, then pushes it to trainerCollection.
+//FUTURE UPDATE: Let the player see the Pokemon's name before deciding what to name it
+function catchPokemon() {
+	var myPokemon = createPokemon();
+	var newName = prompt("Please enter a name for your new Pokemon");
+/*	if (newName.length < 3) {
+		var newName = input("Please enter a name that's at least 3 characters");
+	} */
+	myPokemon.givenName = String(newName);
+	trainerCollection.push(myPokemon);
+};
+
+//Takes an array of pokemon and displays their values
+function displayPokemon(pokemonObjects) {
+	for (i = 0; i < pokemonObjects.length; i++) {
+		var positionInArray = i + 1;
+		console.log(positionInArray + "). " + pokemonObjects[i].species +"\n");
+		console.log("name: " + pokemonObjects[i].givenName + "\n");
+		console.log("level: " + pokemonObjects[i].level + "\n");
+		console.log("XP to next level:" + pokemonObjects[i].XPtoNextLevel + "\n");
+
+	}
+}
+
+catchPokemon();
+
+displayPokemon(trainerCollection);
+
+/*
+//Displays the trainer's collection, and returns the pokemon of their choice
+function chooseTrainerPokemon() {
+	for (i = 0; i < trainerCollection.length; i++) {
+		var pokemonNumber = i + 1
+
+	}
+}
+
+//Takes an integer xpAMount and a pokemonObject - UNFINISHED
+function gainXP(xpAmount, pokemonObject) {
+	var tempTrainerCollection = trainerCollection.slice(0);
+	for (i in tempTrainerCollection)
+		if (pokemonObject === tempTrainerCollection[i].givenName) {
+			chosenPokemonXPGain();
+		} else {
+			console.log("error: the pokemon wasn't found");
+		}
+}
+
+
+
+
+catchPokemon();
+
+displayPokemon(trainerCollection);
+
 
 var myPokemon = createPokemon();
 for (i in myPokemon) {
 	console.log(i, myPokemon[i])
 };
+
+*/
+
+
